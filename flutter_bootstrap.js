37867d454c85a64f7849be52ad8a63e01314b39a
alielbanna <alielbanna@users.noreply.github.com>
2026-02-01 19:07:32 +0000

    deploy: fa74b5215e3e62969d2cc3e01fdd5d0b51ad570a

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"587c18f873b8ab57330422bce09047420d9c7f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4076012641"
+    serviceWorkerVersion: "3863550750"
   }
 });
